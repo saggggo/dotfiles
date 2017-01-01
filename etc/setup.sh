4c5d640734a44101c9ffd6dd2af2d7d56a3aa779
sagggggo <k10700460@yahoo.co.jp>
2017-01-01 20:06:00 +0900

    いろいろと修正

--- etc/setup.sh
+++ etc/setup.sh
@@ -3,7 +3,7 @@
 DOT_PATH=$HOME/dotfiles
 
 #generals
-DOT_FILES=(.vimrc .bashrc .zshenv .zshrc .gitconfig .Xmodmap .xinitrc .xinputrc .alias)
+DOT_FILES=(.vimrc .bashrc .zshenv .zshrc .gitconfig .Xmodmap .xinitrc .xinputrc .aliases)
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
