5bdd78b3f5cde03c6600f76ead354affba8c61ed
saggggo <k10700460@yahoo.co.jp>
2019-04-06 15:44:03 +0900

    hoshi_labo settings

--- bin/utilities/jpgToPng.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in `find . -maxdepth 1 -type f`; do
-    from=`basename ${file}`
-    to=`basename ${file//jpg/png}`
-    echo ${from} "convert to" ${to}
-    convert $file ${file//jpg/png}
-done
